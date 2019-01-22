8b16ac08367c7f8e648a076e35028885d7ed87e5
Stuart Tait <stuart.tait@kingston-college.ac.uk>
2019-01-22 15:49:52 +0000

    Checks for Manifest folder, added user feedback

--- create_manifests.sh
+++ create_manifests.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+
+if [ -d "./Manifests" ]; then
+    echo "Manifests folder already exists."
+else
+    mkdir "./Manifests"
+    echo "Manifests folder created."
+fi
+
 while IFS='' read -r line || [[ -n "$line" ]]; do
 	touch "./Manifests/$line.xml"
 	cat "munki_manifest_template.xml" > "./Manifests/$line.xml"
+    echo "- Created $line.xml"
 done < "$1"
+
+echo "Finished"
